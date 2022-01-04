01acdae9345c8eea7ef7d67ce9a874ee091b6a61
acuraintegurl <39477899+acuraintegurl@users.noreply.github.com>
2022-01-04 20:19:49 +1100

    import portfolio docs

---
+++ script.js
@@ -0,0 +1,19 @@
+function show_else_0() {
+  document.getElementById("role").style.display = "visible";
+  document.getElementById("city").style.visibility = "visible";
+}
+
+function hide_else_0() {
+  document.getElementById("role").style.visibility = "hidden";
+  document.getElementById("city").style.visibility = "hidden";
+}
+
+function show_else_1() {
+  document.getElementById("name").style.visibility = "visible";
+  document.getElementById("role").style.visibility = "visible";
+}
+
+function hide_else_1() {
+  document.getElementById("name").style.visibility = "hidden";
+  document.getElementById("role").style.visibility = "hidden";
+}
