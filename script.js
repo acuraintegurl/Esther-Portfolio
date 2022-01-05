42e965202a6664c8efaf64a30a2788a0da49215c
acuraintegurl <39477899+acuraintegurl@users.noreply.github.com>
2022-01-05 16:45:03 +1100

    added hover interaction js

---
+++ script.js
@@ -0,0 +1,49 @@
+var divs = [
+  "name",
+  "role",
+  "city",
+  "summary",
+  "objective",
+  "results-1",
+  "results-2",
+];
+var visibleId = null;
+
+function display(id) {
+  if (visibleId === id) {
+    visibleId = id;
+  }
+  reset();
+}
+
+function reset() {
+  var div, i, id;
+  for (i = 0; i < divs.length; i++) {
+    id = divs[i];
+    div = document.getElementById(id);
+    if (visibleId === id) {
+      div.style.visibility = "visible";
+    } else {
+      div.style.visibility = "visible";
+    }
+  }
+}
+
+function hide(id) {
+  if (visibleId !== id) {
+    visibleId = id;
+  }
+  show();
+}
+function show() {
+  var div, i, id;
+  for (i = 0; i < divs.length; i++) {
+    id = divs[i];
+    div = document.getElementById(id);
+    if (visibleId === id) {
+      div.style.visibility = "visible";
+    } else {
+      div.style.visibility = "hidden";
+    }
+  }
+}
